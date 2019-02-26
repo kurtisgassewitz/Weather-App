96873d76414117d97ce54b2989e5338a92a42bae
Kgassewitz <kurtis.gassewitz@hotmail.com>
2019-02-26 11:55:06 -0500

    Initial Commit

---
+++ Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Kurtis Gassewitz on 2019-02-26.
+//  Copyright © 2019 KgWitz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
